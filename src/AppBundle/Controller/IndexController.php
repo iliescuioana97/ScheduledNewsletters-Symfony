817565c55893a50db07ebe785424b627d09807aa
iliescuioana97 <iliescuioana97@gmail.com>
2017-02-28 14:56:31 +0200

    Front page changed

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -13,12 +13,33 @@ class IndexController extends Controller {
     public function indexAction() {
         return $this->render('AppBundle:newsletter:index.html.twig');
     }
-    
+
     /**
-     * @Route("/about", name="about")
+     * @Route("/history", name="history")
      */
-    public function aboutAction() {
-        return $this->render('AppBundle:newsletter:about.html.twig');
+    public function historyAction() {
+        return $this->render('AppBundle:newsletter:history.html.twig');
+    }
+
+    /**
+     * @Route("/music", name="music")
+     */
+    public function musicAction() {
+        return $this->render('AppBundle:newsletter:music.html.twig');
+    }
+
+    /**
+     * @Route("/literature", name="literature")
+     */
+    public function literatureAction() {
+        return $this->render('AppBundle:newsletter:literature.html.twig');
+    }
+
+    /**
+     * @Route("/painting", name="painting")
+     */
+    public function paintingAction() {
+        return $this->render('AppBundle:newsletter:painting.html.twig');
     }
 
 }
